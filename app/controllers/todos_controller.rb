d9a15383badd80d4e3e6ac41569bbc92cfa9df3a
tam <tamara.acevedoavila@gmail.com>
2018-06-14 23:24:24 -0400

    casi tarea

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,14 +1,53 @@
 class TodosController < ApplicationController
+  before_action :find_todo, only: [:show, :edit, :update, :destroy, :complete]
   def index
     @todos = Todo.all
   end
 
   def new
-    @todo = Todo.new(params[:todo])
+    @todo = Todo.new
   end
 
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save!
+    redirect_to todos_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @todo.update(todo_params)
+    redirect_to todos_path
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to todos_path
+  end
+
+  def complete
+    @todo.completed = true
+    # @todo.update(todo_params)
+    redirect_to todos_path
+  end
+
+  def list
+    @todos = Todo.all
+    @pendientes = @todos.select  { |item| item[:completed] == false }
+    @completos = @todos.select { |item| item[:completed] == true }
+  end
+
+  def find_todo
+    @todo = Todo.find(params[:id])
+  end
   private
-  def post_params
+
+  def todo_params
     params.require(:todo).permit(:description, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'todos', to: 'todos#index'
   get 'todos/new'
+  post 'todos', to: 'todos#create'
+
+  get 'todos/:id/edit', to: 'todos#edit', as: 'edit'
+  patch 'todos/:id', to: 'todos#update', as: 'todo'
+  delete 'todos/:id', to: 'todos#destroy'
+
+  get 'todos/list', to: 'todos#list'
+  get 'todos/:id', to: 'todos#show'
+  get 'todos/:id', to: 'todos#complete'
+
+  # resources :todos
 end
